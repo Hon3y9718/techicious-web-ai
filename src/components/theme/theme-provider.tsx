0eedb8bc0f18d2d00fbc550d98752a18bdd490eb
Umesh Kumar <uk481281@gmail.com>
2025-08-12 14:40:19 +0000

    Revert last 2 changes

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -1,13 +1,15 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes"
 import type { ThemeProviderProps } from "next-themes/dist/types"
+import { FlyingCreatures } from "./flying-creatures"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider {...props}>
       {children}
+      <FlyingCreatures />
     </NextThemesProvider>
   )
 }
