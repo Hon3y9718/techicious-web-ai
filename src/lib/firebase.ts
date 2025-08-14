690d9d910f36695bfc22b2f8ba8ea5911dea2383
Umesh Kumar <39588540+Hon3y9718@users.noreply.github.com>
2025-08-14 12:41:09 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDLhZiRmmnj1CcbSTUsMn5xCXhav7c4A8U",
+  authDomain: "techicious-website-25e58.firebaseapp.com",
+  projectId: "techicious-website-25e58",
+  storageBucket: "techicious-website-25e58.firebasestorage.app",
+  messagingSenderId: "648388960346",
+  appId: "1:648388960346:web:ab1b59df8f2820fa2a3863",
+  measurementId: "G-NHRP0Q3YN2"
 };
 
 // Initialize Firebase
